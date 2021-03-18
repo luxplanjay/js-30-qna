eb14b0907b89bc1a11e1978ec97a647399a9b27a
Alexander Repeta <alexander.repeta@gmail.com>
2021-03-18 21:33:44 +0200

    add examples

--- js/app.js
+++ js/app.js
@@ -1,42 +1,87 @@
-// class Hero {
-//   constructor(heroname, xp) {
-//     this.heroname = heroname;
-//     this.xp = xp;
-//   }
-// }
+// const vehicles = [
+//   { make: 'Honda', model: 'CR-V', type: 'suv', amount: 14, price: 24045, onSale: true },
+//   { make: 'Honda', model: 'Accord', type: 'sedan', amount: 2, price: 22455, onSale: true },
+//   { make: 'Mazda', model: 'Mazda 6', type: 'sedan', amount: 8, price: 24195, onSale: false },
+//   { make: 'Mazda', model: 'CX-9', type: 'suv', amount: 7, price: 31520, onSale: true },
+//   { make: 'Toyota', model: '4Runner', type: 'suv', amount: 19, price: 34210, onSale: false },
+//   { make: 'Toyota', model: 'Sequoia', type: 'suv', amount: 16, price: 45560, onSale: false },
+//   { make: 'Toyota', model: 'Tacoma', type: 'truck', amount: 4, price: 24320, onSale: true },
+//   { make: 'Ford', model: 'F-150', type: 'truck', amount: 11, price: 27110, onSale: true },
+//   { make: 'Ford', model: 'Fusion', type: 'sedan', amount: 13, price: 22120, onSale: true },
+//   { make: 'Ford', model: 'Explorer', type: 'suv', amount: 6, price: 31660, onSale: false },
+// ];
 
-// class Tank extends Hero {
-//   constructor(heroname, xp, armorvalue) {
-//     super(heroname, xp);
-//     this.armorvalue = armorvalue;
-//   }
-// }
+// // Метод sort - sortByAmount, sortByPrice
+// const sortByAscendingAmount = cars => {
+//   return [...cars].sort((a, b) => a.amount - b.amount);
+// };
+// // console.table(sortByAscendingAmount(vehicles));
 
-// const mango = new Tank("Mango", 1000, 5);
-// console.log(mango);
+// const sortByDescendingPrice = cars => {
+//   return [...cars].sort((a, b) => b.price - a.price);
+// };
+// // console.table(sortByDescendingPrice(vehicles));
 
-// class User {
-//   constructor(firstName, lastName) {
-//     this._firstName = firstName;
-//     this._lastName = lastName;
-//   }
+// // filterByAmount + только название модели
+// // console.table(vehicles.filter(vehicle => vehicle.amount >= 12).map(vehicle => vehicle.model));
 
-//   get fullName() {
-//     return `${this._firstName} ${this._lastName}`;
-//   }
+// // console.table(
+// //   vehicles.reduce((result, vehicle) => {
+// //     if (vehicle.amount >= 12) {
+// //       return [...result, vehicle.model];
+// //     }
+// //     return result;
+// //   }, []),
+// // );
 
-//   set fullName(name) {
-//     const [first, last] = name.split(" ");
-//     this._firstName = first;
-//     this._lastName = last;
-//   }
-// }
+// // filter + sort
+// // console.table(vehicles.filter(vehicle => vehicle.onSale).sort((a, b) => a.price - b.price));
 
-// const user = new User("Jacob", "Mercer");
-// console.log(user.fullName);
-// user.fullName = "Adrian Cross";
-// console.log(user.fullName);
+// // Метод map - getModels
+// const getModels = cars => {
+//   return cars.map(car => car.model);
+// };
+// // console.table(getModels(vehicles));
 
-// const button = document.querySelector("button");
-// button.classList.add("super");
-// button.textContent = "Спасибо вам умные люди :)";
+// // Метод filter - filterByAmount, filterByPrice, getCarsWithDiscount, getCarsWithType
+
+// const filterByAmount = (cars, amountThreshold) => {
+//   return cars.filter(car => car.amount >= amountThreshold);
+// };
+// // console.table(filterByAmount(vehicles, 10));
+// // console.table(filterByAmount(vehicles, 15));
+
+// const filterByPrice = (cars, priceThreshold) => {
+//   return cars.filter(car => car.price < priceThreshold);
+// };
+// // console.table(filterByPrice(vehicles, 25000));
+// // console.table(filterByPrice(vehicles, 23000));
+
+// const getCarsWithDiscount = cars => {
+//   return cars.filter(car => car.onSale);
+// };
+// // console.table(getCarsWithDiscount(vehicles));
+
+// const getCarsWithType = (cars, type) => {
+//   return cars.filter(car => car.type === type);
+// };
+// // console.table(getCarsWithType(vehicles, 'suv'));
+
+// //  А если хочу отфильтровать и те что на sale и те что дешевле 23к,просто добовлять нескольео условий?или надо два раза фильтр делать?
+
+// const getCarsOnSaleAndWithPrice = (cars, priceThreshold) => {
+//   return cars.filter(car => car.price < priceThreshold && car.onSale);
+// };
+// // console.table(getCarsOnSaleAndWithPrice(vehicles, 23000));
+
+// // Метод find - getCarByModel
+// const getCarByModel = (cars, model) => {
+//   return cars.find(car => car.model === model);
+// };
+// // console.log(getCarByModel(vehicles, 'F-150'));
+
+// // Метод reduce - getTotalPrice
+// const getTotalCarsAmount = cars => {
+//   return cars.reduce((total, car) => total + car.amount, 0);
+// };
+// // console.log(getTotalCarsAmount(vehicles));
