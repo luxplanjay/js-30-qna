041bd6e438036a3637a609a609d4ae26b7dc2733
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-27 21:01:11 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,35 @@
-import { fetchMovies } from './services/movie-service';
-import movieListTpl from '../templates/movie-list.hbs';
+import * as basicLightbox from 'basiclightbox';
+import { Spinner } from 'spin.js';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import 'spin.js/spin.css';
 import '../sass/main.scss';
-import axios from 'axios';
 
-// 1. Сделать http-запрос
-// 2. Получить и обработать ответ
-// 3. Нарисовать интерфейс
+// const opts = {
+//     lines: 12, // The number of lines to draw
+//     length: 24, // The length of each line
+//     width: 17, // The line thickness
+//     radius: 45, // The radius of the inner circle
+//     scale: 1, // Scales overall size of the spinner
+//     corners: 1, // Corner roundness (0..1)
+//     speed: 1, // Rounds per second
+//     rotate: 0, // The rotation offset
+//     animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+//     direction: 1, // 1: clockwise, -1: counterclockwise
+//     color: 'tomato', // CSS color or array of colors
+//     fadeColor: 'transparent', // CSS color or array of colors
+//     top: '50%', // Top position relative to parent
+//     left: '50%', // Left position relative to parent
+//     shadow: '0 0 1px transparent', // Box-shadow for the lines
+//     zIndex: 2000000000, // The z-index (defaults to 2e9)
+//     className: 'spinner', // The CSS class to assign to the spinner
+//     position: 'absolute', // Element positioning
+// };
 
-const movieListRoot = document.querySelector('#movie-list-root');
-
-const renderMovieList = movies => {
-    const markup = movieListTpl(movies);
-    movieListRoot.insertAdjacentHTML('beforeend', markup);
-};
-
-const handleFetchMoviesError = error => {
-    if (error.message === 'Request failed with status code 404') {
-        console.log('Упс, что-то пошло не так :(');
-    }
-};
-
-// console.log('Показываем спинер');
-// fetchMovies()
-//     .then(movies => renderMovieList(movies))
-//     .catch(handleFetchMoviesError)
-//     .finally(() => console.log('Прячем спинер'));
-
-// async function init() {
-//     console.log('Показываем спинер');
-
-//     try {
-//         const movies = await fetchMovies();
-//         renderMovieList(movies);
-//     } catch (error) {
-//         handleFetchMoviesError(error);
-//     } finally {
-//         console.log('Прячем спинер');
-//     }
-// }
-
-// init();
+// const instance = basicLightbox.create(`
+// 	<h1>Dynamic Content</h1>
+// 	<p>You can set the content of the lightbox with JS.</p>
+//   <div id="spinner-root"></div>
+// `);
+// instance.show();
+// const spinnerRoot = instance.element().querySelector('#spinner-root');
+// const spinner = new Spinner(opts).spin(spinnerRoot);
